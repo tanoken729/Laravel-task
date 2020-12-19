53b64ff23612450462d7827bfde57366ef99464e
たのけん <tanoken729@gmail.com>
2020-12-19 18:43:02 +0900

    ログイン画面のテンプレート作成

---
+++ resources/views/login.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form action="/register">
+    <p>E-Mail Address：<input type="mail"></p>
+    <p>Password：<input type="password"></p>
+    <p><input type="radio">Remember me</p>
+    <p><input type="submit" value="Login"> <a href="">Forget Your Password</a></p>
+    </form>
+</body>
+</html>
