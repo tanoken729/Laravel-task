e60ca5176e9c4c33dcacb0ccf02457e7bce4bb91
たのけん <tanoken729@gmail.com>
2020-12-19 18:46:17 +0900

    ユーザー登録画面のテンプレートを作成

---
+++ resources/views/register.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+<body>
+    <form action="/login">
+    <p>Name：<input type="text"></p>
+    <p>E-Mail Address：<input type="mail"></p>
+    <p>Password：<input type="password"></p>
+    <p>Confirm Password：<input type="password"></p>
+    <p><input type="submit" value="Register"></p>
+    </form>
+</body>
+</html>
