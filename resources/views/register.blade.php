a836fa4985c34264e3230b673095e831763ae19b
たのけん <tanoken729@gmail.com>
2020-12-19 19:29:23 +0900

    ユーザー登録画面のフォームををテーブル表示にした

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -7,11 +7,12 @@
 </head>
 <body>
     <form action="/login">
-    <p>Name：<input type="text"></p>
-    <p>E-Mail Address：<input type="mail"></p>
-    <p>Password：<input type="password"></p>
-    <p>Confirm Password：<input type="password"></p>
-    <p><input type="submit" value="Register"></p>
+    <table border=1>
+    <tr><th>Name：</th><td><input type="text"></td></tr>
+    <tr><th>E-Mail Address：</th><td><input type="mail"></td></tr>
+    <tr><th>Password：</th><td><input type="password"></td></tr>
+    <tr><th>Confirm Password：</th><td><input type="password"></td></tr>
+    <tr><th></th><td><input type="submit" value="Register"></td></tr>
     </form>
 </body>
 </html>
