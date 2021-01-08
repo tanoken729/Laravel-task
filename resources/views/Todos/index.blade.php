ae311555e89d0ca41b0ffd8819b139cb8c32151f
たのけん <tanoken729@gmail.com>
2021-01-09 03:50:13 +0900

    formの閉じタグの位置を修正した

--- resources/views/Todos/index.blade.php
+++ resources/views/Todos/index.blade.php
@@ -14,10 +14,18 @@
     @endif
 <h1>Todoリスト</h1>
         <table>
-        <tr><td><input type="radio"></td><th>すべて</th><td><input type="radio"></td><th>作業中</th><td><input type="radio"></td><th>完了</th>
+            <tr>
+                <td><input type="radio"></td><th>すべて</th>
+                <td><input type="radio"></td><th>作業中</th>
+                <td><input type="radio"></td><th>完了</th>
+            </tr>
         </table>
         <table>
-        <tr><th>ID</th><th>コメント</th><th>状態</th></tr>
+            <tr>
+                <th>ID</th>
+                <th>コメント</th>
+                <th>状態</th>
+            </tr>
         @foreach($todos as $todo)
         <tr>
             <!-- findで検索されたIDを取得 -->
@@ -26,19 +34,31 @@
             <td>{{$todo->comment}}</td>
             @if($todo->state > 0)
             <!-- 作業中ボタン -->
-            <td><input type="submit" value="作業中" name=""></td>
+            <td><input type="button" value="作業中" onclick="Display_JS('作業中')"></td>
+            <!-- <input type="button" name="task" onclick="Display_JS('作業中')"value="{{$todo->state}}"> -->
             @else
             <!-- 完了ボタン -->
-            <td><input type="submit" value="完了" name=""></td>
+            <td><input type="button" value="完了" onclick="Display_JS('完了')"></td>
+            <!-- <input type="hidden" name="finish" value="{{$todo->state}}"> -->
             @endif
+            <!-- <script>
+            function Display_JS(a)
+            {
+                if(a == "作業中"){
+                    document.getElementById("task").defaultValue =  "0";
+                }else if(a == "完了"){
+                    document.getElementById("finish").defaultValue =  "1";
+                }
+            }
+            </script> -->
         <form action="{{url('/todos', $todo->id)}}" method="POST">
         @csrf
             <!-- 削除ボタン -->
             <td><button type="submit">削除</button></td>
         </tr>
+        </form>
         @endforeach
         </table>
-        </form>
 <h1>新規タスクの追加</h1>
 <form action="todos" method="POST">
 @csrf
