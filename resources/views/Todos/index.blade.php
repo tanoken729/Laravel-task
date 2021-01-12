5a8e4502f49e0cbea0f5c48c4b51a9089f3463c6
たのけん <tanoken729@gmail.com>
2021-01-12 15:01:27 +0900

    インデントの修正

--- resources/views/Todos/index.blade.php
+++ resources/views/Todos/index.blade.php
@@ -3,59 +3,59 @@
 @section('title', 'Todoリスト')
 
 @section('content')
-    @if(count($errors) > 0)
+  @if(count($errors) > 0)
     <div>
-        <ul>
-            @foreach($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+      <ul>
+        @foreach($errors->all() as $error)
+          <li>{{ $error }}</li>
+        @endforeach
+      </ul>
     </div>
-    @endif
+   @endif
 <h1>Todoリスト</h1>
-                <label><input type="radio" checked>すべて</label>
-                <label><input type="radio">作業中</label>
-                <label><input type="radio">完了</label>
-        <table>
-            <tr>
-                <th>ID</th>
-                <th>コメント</th>
-                <th>状態</th>
-            </tr>
-        @foreach($todos as $todo)
+  <label><input type="radio" checked>すべて</label>
+  <label><input type="radio">作業中</label>
+  <label><input type="radio">完了</label>
+    <table>
+      <tr>
+        <th>ID</th>
+        <th>コメント</th>
+        <th>状態</th>
+      </tr>
+      @foreach($todos as $todo)
         <tr>
         <!-- findで検索されたIDを取得 -->
-            <input type="hidden" name="id" value="{{$todo->id}}">
-            <td>{{$loop->iteration}}</td>
-            <td>{{$todo->comment}}</td>
-            @if($todo->state > 0)
-        <!-- 作業中ボタン -->
-            <form action="{{url('/todos', $todo->id)}}" method="POST">
+          <input type="hidden" name="id" value="{{$todo->id}}">
+          <td>{{$loop->iteration}}</td>
+          <td>{{$todo->comment}}</td>
+          @if($todo->state > 0)
+          <form action="{{url('/todos', $todo->id)}}" method="POST">
             @csrf
+            <!-- 作業中ボタン -->
             <td><input type="submit" value="作業中"></td>
             <input type="hidden" name="state" value="0">
-            </form>
-        　　 @else
-        <!-- 完了ボタン -->
-            <form action="{{url('/todos', $todo->id)}}" method="POST">
+          </form>
+          @else
+          <form action="{{url('/todos', $todo->id)}}" method="POST">
             @csrf
+            <!-- 完了ボタン -->
             <td><input type="submit" value="完了"></td>
             <input type="hidden" name="state" value="1">
-            </form>
-            @endif
-            <form action="{{url('/todos', $todo->id)}}" method="POST">
+          </form>
+          @endif
+          <form action="{{url('/todos', $todo->id)}}" method="POST">
             @csrf
-        <!-- 削除ボタン -->
+            <!-- 削除ボタン -->
             <td><button type="submit">削除</button></td>
-            </tr>
-            </form>
-        @endforeach
-        </table>
+          </form>
+        </tr>
+      @endforeach
+    </table>
 <h1>新規タスクの追加</h1>
 <form action="todos" method="POST">
-@csrf
-            <input type="text" name="comment" value="{{old('comment')}}">
-            <!-- 追加ボタン -->
-            <input type="submit" value="追加">
-    </form>
+  @csrf
+    <input type="text" name="comment" value="{{old('comment')}}">
+    <!-- 追加ボタン -->
+    <input type="submit" value="追加">
+</form>
 @endsection
